7530ae6fbaae84411b388fdda0d354cee5812c74
Francieli A. Persch <41474400+francielip@users.noreply.github.com>
2026-01-25 14:13:51 +0000

    Criação do arquivo app.py e atualização do README.md

---
+++ app.py
@@ -0,0 +1,21 @@
+print("""
+█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
+▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+""")
+print('1. Cadastrar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+print(f'Voce escolheu a opcao {opcao_escolhida}.')
+#interpolacao de strings
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa.')
