524127ee60612d5641d57f37bd407da8887d981d
Francieli A. Persch <41474400+francielip@users.noreply.github.com>
2026-01-26 15:17:22 +0000

    Atualização do app.py para adicionar funções de cadastro e listagem de restaurantes, além de melhorias na estrutura de controle de fluxo.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Projeto para aplicativo de entrega de comida
 import os
 
+# Criação da lista de restaurantes
+restaurantes = ['Casa do Pão','Espetinhos do João','Sucos da Vale']
+
 # Criando funções com def
 def exibir_nome_do_app():
     print("""
@@ -16,15 +19,38 @@ def menu_prinpical():
     # Quebra de linha usando \n
 
 def finalizar_app():
+    exibir_subtitulo('Finalizando o app.')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal.')
+    main()
+
+def exibir_subtitulo(texto):
     os.system('clear')
-    print('Finalizando o app.\n')
+    print(texto)
+    print()
 
 def opcao_invalida():
     print('Opção Inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal.')
-    main()
+    voltar_ao_menu_principal()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    # Para adicionar o restaurante à lista, usamos append
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso.')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes cadastrados')
+    # Estrutura de repetição com for
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
+    # Try para controle de execução
     try:
         # Conversão de tipo de string para inteiro usando int()
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -32,9 +58,9 @@ def escolher_opcao():
         print(f'Voce escolheu a opcao {opcao_escolhida}.')
         # Estrutura condicional usando if, elif e else  
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
