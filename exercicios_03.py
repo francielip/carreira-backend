456111ea4818f3e69c15a89368317fe6eaf17b1d
Francieli A. Persch <41474400+francielip@users.noreply.github.com>
2026-01-26 15:24:43 +0000

    Adição do arquivo exercicios_03.py com exercícios de criação e manipulação de listas, incluindo loops e tratamento de exceções.

---
+++ exercicios_03.py
@@ -0,0 +1,25 @@
+# 1 - Crie uma lista para cada informação a seguir:
+# Lista de números de 1 a 10;
+# Lista com quatro nomes;
+# Lista com o ano que você nasceu e o ano atual.
+print('\n_____ Exercício 01 _____')
+print('>>> Criando listas e exibindo elas\n')
+numeros =['1','2','3','4','5','6','7','8','9','10']
+nomes = ['Francieli','Ricardo','Leandro']
+anos = ['1984','2026']
+print(numeros)
+print(nomes)
+print(anos)
+
+# 2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+print('\n_____ Exercício 02 _____')
+print('>>> Criando lista com loop\n')
+cores = ['Preto','Branco','Azul']
+for cor in cores:
+    print(f'Cor: {cor}')
+
+# 3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+# 4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
+# 5 - Solicite ao usuário um número e, em seguida, utilize um loop for para imprimir a tabuada desse número, indo de 1 a 10.
+# 6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. Utilize um bloco try-except para lidar com possíveis exceções.
+# 7 - Construa um código que calcule a média dos valores em uma lista. Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
