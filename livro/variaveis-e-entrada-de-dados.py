c90c7494a7c1861ccadc6347ab4e2aa359155311
Francieli A. Persch <41474400+francielip@users.noreply.github.com>
2026-03-28 18:04:38 +0000

    Exercício do livro sobre variáveis e entrada de dados.

---
+++ livro/variaveis-e-entrada-de-dados.py
@@ -0,0 +1,9 @@
+# Exercício 3.7 Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+# Exercício 3.8 Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
+# Exercício 3.9 Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
+# Exercício 3.10 Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do novo salário.
+# Exercício 3.11 Faça um programa que solicite o preço de uma mercadoria e o percentual de desconto. Exiba o valor do desconto e o preço a pagar.
+# Exercício 3.12 Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+# Exercício 3.13 Escreva um programa que converta uma temperatura digitada em °C em °F. A fórmula para essa conversão é:$$F = \frac{9 \times C}{5} + 32$$
+# Exercício 3.14 Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por km rodado.
+# Exercício 3.15 Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro, e calcule quantos dias de vida um fumante perderá. Exiba o total em dias.
